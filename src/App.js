ebd8b8d6b936ac872f10b54b989be12e514625fc
Anthony Yam <anthonysapien@gmail.com>
2020-04-12 16:39:45 -0400

    debugging time ago

--- src/App.js
+++ src/App.js
@@ -112,7 +112,7 @@ const DonationInfo = ({ donation }) => {
   const date = new Date(donation.createdAt);
   const timeAgo = moment(date).fromNow();
   return (
-    <div className='info'>{donation.createdAt},{date},{timeAgo}</div>    
+    <div className='info'><span>{donation.createdAt}</span><br/><span>{date.toString()}</span><br/><span>{timeAgo}</span></div>    
   );
 };
 
